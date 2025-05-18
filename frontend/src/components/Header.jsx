d0995edc3d603f5aa515a0cc273dc30fe105e00b
Malik <malik.warre@icloud.com>
2025-05-17 22:13:25 -0400

    backend

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
-import { Navbar, Nav, Container} from 'react-bootstrap';
-import {FaShoppingCart, FaUser} from 'react-icons/fa';
-import { LinkContainer} from 'react-router-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 const Header = () => {
@@ -8,28 +8,21 @@ const Header = () => {
     <header>
         <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
             <Container>
-                <LinkContainer to='/'>
-                <Navbar.Brand >
-                <img src={logo} alt='MalikShop'></img>
-                MalikShop
+                <Navbar.Brand as={Link} to='/'>
+                    <img src={logo} alt='MalikShop'></img>
+                    MalikShop
                 </Navbar.Brand>
-                </LinkContainer>
                 <Navbar.Toggle aria-controls='basic-navbar-nav' />
                 <Navbar.Collapse id='basic-navbar-nav'>
                     <Nav className='ms-auto'>
-                        <LinkContainer to='/cart'>
-                        <Nav.Link>
-                            <FaShoppingCart/>Cart
+                        <Nav.Link as={Link} to='/cart'>
+                            <FaShoppingCart/> Cart
                         </Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to='/login'>
-                        <Nav.Link href='/login'>
-                            <FaUser/>Sign In
+                        <Nav.Link as={Link} to='/login'>
+                            <FaUser/> Sign In
                         </Nav.Link>
-                        </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
-
             </Container>
         </Navbar>
     </header>
