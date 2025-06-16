326dba0e319c775500cd6fe8cf091ee8a50621ba
Malik <malik.warre@icloud.com>
2025-06-15 20:00:16 -0400

    page title and depolyment

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -14,6 +14,7 @@ import {
 import Rating from '../components/Rating';
 import {toast} from 'react-toastify';
 import Loader from '../components/Loader';
+import {Meta} from '../components/Meta';
 import {
   useGetProductDetailsQuery,
   useCreateReviewMutation,
@@ -83,6 +84,7 @@ const ProductScreen = () => {
         </Message>
       ) : (
         <>
+          <Meta title={product.name} />
           <Row>
             <Col md={5}>
               <Image
